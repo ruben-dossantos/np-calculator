742cee7ddff0e30dc45993b099fb80d2444fe628
Ruben Barros <ruben.23@live.com.pt>
2013-03-20 15:12:03 +0000

    Mais calculadoras

--- src/main/scala/Calculator/Calc.scala
+++ src/main/scala/Calculator/Calc.scala
@@ -3,24 +3,41 @@ package Calculator
 import collection.mutable
 
 
-object Calc {
+
+object Calc{
   val stack = new mutable.Stack[Double]
 
 
-  def push(num:Double){
-    stack.push(num)
-
-  }
+  def push(num:Double) = stack.push(num)
 
   def op(op:Char):Option[Double] = {
     val num2 = stack.pop()
     val num1 = stack.pop()
+    var result:Double = 0
     op match{
-      case '+' => Some(num1+num2)
-      case '*' => Some(num1*num2)
-      case '-' => Some(num1-num2)
-      case '/' => Some(num1/num2)
+      case '+' =>
+        result=num1+num2
+        push(result)
+        Some(result)
+      case '*' =>
+        result=num1*num2
+        push(result)
+        Some(result)
+      case '-' =>
+        result=num1-num2
+        push(result)
+        Some(result)
+      case '/' =>
+        result=num1/num2
+        push(result)
+        Some(result)
       case _ => None
     }
+  }
+
+  def clean(){
+    do {
+      stack.pop()
+    }while(!stack.isEmpty)
   }
 }
--- src/test/scala/cucumber/CalculatorTest.scala
+++ src/test/scala/cucumber/CalculatorTest.scala
@@ -3,14 +3,16 @@ package cucumber
 import api.scala.{ScalaDsl, EN}
 import org.junit.Assert._
 import Calculator.Calc
-
+import Calculator.MultipleCalc
+import scala.Some
 
 
 class CalculatorTest extends ScalaDsl with EN {
 
   var result:Option[Double] = None
 
-  Given("""^a number (\d+)$"""){ (arg0:Int) =>
+
+  Given("""^a number (-?\d+)$"""){ (arg0:Int) =>
     Calc.push(arg0)
   }
 
@@ -35,22 +37,54 @@ class CalculatorTest extends ScalaDsl with EN {
     result = Calc.op('/')
   }
 
-  Then("""^result should be (\d+)$"""){ (arg0:Int) =>
+  Then("""^result should be (-?\d+)$"""){ (arg0:Int) =>
+    Calc.clean()
     result match{
       case Some(num)=>assertEquals(num,arg0,0)
       case None => throw new Exception("Resultado invalido!")
     }
 
   }
-  Then("""^result is (\d+)$"""){ (arg0:Int) =>
+  Then("""^result is (-?\d+)$"""){ (arg0:Int) =>
+    Calc.clean()
     result match{
       case Some(num)=>assertEquals(num,arg0,0)
       case None => throw new Exception("Resultado invalido!")
     }
+
   }
 
 
+  var map: Map[String,MultipleCalc] = Map()
+  var results:Map[String,Option[Double]] = Map()
 
+  Given("""^a calculator named "([^"]*)"$"""){ (arg0:String) =>
+    map+=(arg0->new MultipleCalc)
+  }
+
+  When("""^a number (-?\d+) is sent to "([^"]*)"$"""){ (arg0:Int, arg1:String) =>
+    val arg=arg1
+    val mc:MultipleCalc = map(arg)
+    mc.push(arg0)
+  }
+
+  When("""^an operation "([^"]*)" is sent to "([^"]*)"$"""){ (arg0:Char, arg1:String) =>
+    val arg=arg1
+    val mc:MultipleCalc = map(arg)
+    val result= mc.op(arg0)
+    results+=(arg->result)
+  }
+
+  Then("""^the result in "([^"]*)" is (\d+)$"""){ (arg0:String, arg1:Int) =>
+    val arg=arg0
+    val mc:MultipleCalc = map(arg)
+
+    mc.clean()
+    results(arg) match{
+      case Some(num)=>assertEquals(num,arg1,0)
+      case None => throw new Exception("Resultado invalido!")
+    }
+  }
 
 
 
